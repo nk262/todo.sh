94f8b0794f2565427fd468faba1c1b3eef753da0
nk262 <NimuK@protonmail.com>
2023-06-30 17:52:34 +0900

    todo.sh

--- todo.sh
+++ todo.sh
@@ -22,7 +22,7 @@ cat << EOF
   usage:
     $0 [command] [args]
   commands:
-    help list add del clear
+    help list add del move status tag clear
 EOF
 }
 
@@ -48,7 +48,7 @@ add_todo() {
   todo_stat="t"
   todo_date="$(date "+%y-%m-%d_%H:%M:%S")"
   todo_tag="#"
-  todo_text="$2"
+  todo_text="$(echo " $2" | sed -z "s/\n//g" | cut -c2-)"
   todo_line="$todo_stat $todo_date $todo_tag $todo_text"
   
   if [ $# -eq 3 ]; then
@@ -62,6 +62,33 @@ add_todo() {
   fi
 }
 
+# change todo tag
+change_tag_todo() {
+  if ! [ $# -eq 3 ]; then
+    echo "Wrong number of arguments."
+    exit 1
+  fi
+  if ! is_number "$3" ; then
+    echo "Is NaN."
+    exit 1
+  fi
+  tag_text="$(echo "=$2" | sed -z "s/[\n ]//g" | cut -c2-)"
+  edit_line="$(echo "$(cat "$TODO" | sed -n "${3}p")")"
+  
+  todo_stat="$(echo "$edit_line" | cut -d" " -f1)"
+  
+  todo_date="$(echo "$edit_line" | cut -d" " -f2)"
+  
+  todo_tag="$tag_text"
+  
+  todo_text="$(echo "$edit_line" | cut -d" " -f4-)"
+  
+  todo_line="$todo_stat $todo_date #$todo_tag $todo_text"
+  
+  echo "$(cat "$TODO" | sed "${3}c$todo_line")" > "$TODO"
+  
+}
+
 # delete todo
 del_todo() {
   if ! [ $# -eq 2 ]; then
@@ -70,6 +97,54 @@ del_todo() {
   fi
   if is_number "$2"; then
     echo "$(cat "$TODO" | sed "${2}d")" > "$TODO"
+  else
+    echo "Is NaN."
+    exit 1
+  fi
+}
+
+# move todo
+move_todo() {
+  if ! [ $# -eq 3 ]; then
+    echo "Wrong number of arguments."
+    exit 1
+  fi
+  if is_number "$2" && is_number "$3"; then
+    if ! [ $2 -eq $3 ]; then
+      move_line="$(echo "$(cat "$TODO" | sed -n "${2}p")")"
+      if [ $2 -gt $3 ]; then
+        echo "$(cat "$TODO" | sed "${2}d")" > "$TODO"
+        echo "$(cat "$TODO" | sed "${3}i${move_line}")" > "$TODO"
+      else
+        echo "$(cat "$TODO" | sed "${3}a${move_line}")" > "$TODO"
+        echo "$(cat "$TODO" | sed "${2}d")" > "$TODO"
+      fi
+    fi
+  else
+    echo "Is NaN."
+    exit 1
+  fi
+}
+
+# change status todo
+status_todo() {
+  if ! [ $# -eq 3 ]; then
+    echo "Wrong number of arguments."
+    exit 1
+  fi
+  if is_number "$2"; then
+    case "$3" in
+      [dD]* )
+        echo "$(cat "$TODO" | sed "${2}s/^t/d/")" > "$TODO"
+        ;;
+      [tT]* )
+        echo "$(cat "$TODO" | sed "${2}s/^d/t/")" > "$TODO"
+        ;;
+      * )
+        echo "Error!"
+        exit 1
+        ;;
+    esac
   else
     echo "Is NaN."
     exit 1
@@ -108,13 +183,15 @@ list_todo() {
     todo_text="$(echo "$line" | cut -d" " -f4-)"
     [ -z "$todo_text" ] && continue
     
-    printf "($idx) "
-    printf "$todo_stat : "
-    printf "[$todo_text"
+    printf "($idx)"
+    printf "[$todo_stat]"
+    printf "{$todo_text"
     yes " " | head -n $(($max_len-${#todo_text})) | tr -d "\n"
-    printf "]"
-    printf " # $todo_tag"
-    printf " : $todo_date"
+    printf "}"
+    if ! [ -z "$todo_tag" ]; then
+      printf "#$todo_tag#"
+    fi
+    printf "<$todo_date>"
     echo
     
     idx=$(($idx+1))
@@ -128,6 +205,9 @@ main() {
     h* | -h* | --h* ) usage ;;
     a* ) add_todo "$@" ;;
     d* ) del_todo "$@" ;;
+    m* ) move_todo "$@" ;;
+    s* ) status_todo "$@" ;;
+    t* ) change_tag_todo "$@" ;;
     c* ) clear_todo ;;
     l* ) list_todo ;;
     * )
